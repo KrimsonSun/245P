ef1396fbcc7e524131ad1afaedeed35cda0ad7c0
KrimsonSun <yijus12@uci.edu>
2025-11-09 13:26:12 -0800

    updateex8

---
+++ ex9/client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>https://github.com/KrimsonSun/245P </p>
+    </div>
+  );
+}
+
+export default Footer;
